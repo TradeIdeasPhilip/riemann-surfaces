a3ffd546d1ee3178bdd89d1a0980e186471ab6ae
Philip Smolen <philip@trade-ideas.com>
2022-08-20 22:42:38 -0700

    You can actually see the points move.
    Click the mouse where you want to move the input.
    Many TODO items!

--- README.md
+++ README.md
@@ -6,13 +6,20 @@ That code doesn't want to run any more, so I'm redoing it in JavaScript (TypeScr
 
 This is a way of graphing a function with complex inputs and outputs.
 It is especially useful for multi-valued functions.
-E.g. ∜16 = 2 __and__ -2.
-If you believe in imaginary numbers, it __also__ equals 2𝓲 and -2𝓲.
+E.g. ∜16 = 2 **and** -2.
+If you believe in imaginary numbers, it **also** equals 2𝓲 and -2𝓲.
+
+## Terminology
+
+For a given 𝒛 there can be multiple 𝒘's.
+For a given 𝒘 there can be only one 𝒛.
+E.g. 𝒛=𝒘².
 
 ## Keywords
 
-* Riemann surface
-* Roots of complex equations
-* Analytic continuation
-* Multi valued functions
-* Complex analysis
+- Riemann surface
+- Roots of complex equations
+- Analytic continuation
+- Multi valued functions
+- Complex analysis
+- Graphing complex functions
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,193 @@
-import './style.css'
+import "./style.css";
 import { Complex } from "complex.js";
+import { getById } from "phil-lib/client-misc";
+import { zip } from "phil-lib/misc";
 
+console.log(new Complex(1, 2), Complex.E, Complex.EPSILON, Complex.INFINITY);
 
-console.log(new Complex(1,2), Complex.E, Complex.EPSILON, Complex.INFINITY);
+// TODO shouldn't this move to phil-lib/client-misc.ts.
+function* permutations<T>(
+  toPermute: readonly T[],
+  prefix: readonly T[] = []
+): Generator<readonly T[], void, undefined> {
+  if (toPermute.length == 0) {
+    yield prefix;
+  } else {
+    for (let index = 0; index < toPermute.length; index++) {
+      const nextItem = toPermute[index];
+      const newPrefix = [...prefix, nextItem];
+      const stillNeedToPermute = [
+        ...toPermute.slice(0, index),
+        ...toPermute.slice(index + 1),
+      ];
+      yield* permutations(stillNeedToPermute, newPrefix);
+    }
+  }
+}
+//console.log(Array.from(permutations(["A", "B", "C"])), Array.from(permutations([1 , 2, 3, 4])), Array.from(permutations([])));
+
+const formula = {
+  shortName: "Square Root",
+  allWs(z: Complex): Complex[] {
+    const primary = z.sqrt();
+    return [primary, primary.neg()];
+  },
+  error(w: Complex, z: Complex): number {
+    return w.pow(2).sub(z).abs();
+  },
+  badPoints: [Complex.ZERO],
+  initialZ: new Complex(4),
+};
+
+const bottomGroup = getById("bottom", SVGGElement);
+const mainGroup = getById("main", SVGGElement);
+const topGroup = getById("top", SVGGElement);
+
+class PathInfo {
+  readonly start: SVGRectElement;
+  readonly end: SVGCircleElement;
+  readonly lastSegment: SVGLineElement;
+  #currentValue: Complex;
+  get currentValue() {
+    return this.#currentValue;
+  }
+  set currentValue(newValue: Complex) {
+    const x = newValue.re;
+    const y = -newValue.im;
+    const line = this.lastSegment;
+    line.x2.baseVal.value = x;
+    line.y2.baseVal.value = y;
+    const circle = this.end;
+    circle.cx.baseVal.value = x;
+    circle.cy.baseVal.value = y;
+    this.#currentValue = newValue;
+  }
+  #undo: Complex;
+  undo() {
+    this.currentValue = this.#undo;
+  }
+  constructor(public readonly color: string, initialValue: Complex) {
+    this.#currentValue = initialValue;
+    this.#undo = initialValue;
+    const x = initialValue.re;
+    const y = -initialValue.im;
+    // <circle cx="2" cy="0" r="0.2" style="--base-color: red"></circle>
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.cx.baseVal.value = x;
+    circle.cy.baseVal.value = y;
+    circle.r.baseVal.value = 0.2;
+    circle.style.setProperty("--base-color", color);
+    topGroup.appendChild(circle);
+    this.end = circle;
+    // <rect x="3.75" y="-0.25" width="0.5" height="0.5" style="--base-color: black"></rect>
+    const square = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "rect"
+    );
+    const size = 0.5;
+    square.x.baseVal.value = x - size / 2;
+    square.y.baseVal.value = y - size / 2;
+    square.width.baseVal.value = size;
+    square.height.baseVal.value = size;
+    square.style.setProperty("--base-color", color);
+    bottomGroup.appendChild(square);
+    this.start = square;
+    // <line x1="4" y1="0" x2="3" y2="-1.5" stroke="black"></line>
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.x1.baseVal.value = x;
+    line.y1.baseVal.value = y;
+    line.x2.baseVal.value = x;
+    line.y2.baseVal.value = y;
+    line.setAttribute("stroke", color);
+    mainGroup.appendChild(line);
+    this.lastSegment = line;
+  }
+}
+
+function init() {
+  mainGroup.innerHTML = "";
+  topGroup.innerHTML = "";
+  for (const badPoint of formula.badPoints) {
+    //        <circle class="bad-point" cx="0" cy="0" r="0.1"></circle>
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.classList.add("bad-point");
+    circle.cx.baseVal.value = badPoint.re;
+    circle.cy.baseVal.value = -badPoint.im;
+    circle.r.baseVal.value = 0.1;
+    mainGroup.appendChild(circle);
+  }
+  const z = formula.initialZ;
+  const zPath = new PathInfo("black", z);
+  const wValues = formula.allWs(z);
+  const wPaths = wValues.map((w, index) => {
+    const color = `hsl(${index / wValues.length}turn, 100%, 50%)`;
+    return new PathInfo(color, w);
+  });
+  return { zPath, wPaths };
+}
+
+let { zPath, wPaths } = init();
+
+function updateZ(z: Complex) {
+  zPath.currentValue = z;
+  let bestChange = Infinity;
+  let bestWsInOrder!: readonly Complex[];
+  for (const wsInOrder of permutations(formula.allWs(z))) {
+    let change = 0;
+    for (const [w, path] of zip(wsInOrder, wPaths)) {
+      change += w.sub(path.currentValue).abs();
+    }
+    if (change < bestChange) {
+      bestChange = change;
+      bestWsInOrder = wsInOrder;
+    }
+  }
+  for (const [w, path] of zip(bestWsInOrder, wPaths)) {
+    path.currentValue = w;
+  }
+}
+
+(window as any).updateZ = (re: number, im: number) => {
+  updateZ(new Complex(re, im));
+};
+
+const svg = document.querySelector("svg")!;
+
+// https://stackoverflow.com/a/10298843/971955
+// Create an SVGPoint for future math
+const pt = svg.createSVGPoint();
+// Get point in global SVG space
+function cursorToComplex(evt: MouseEvent) {
+  pt.x = evt.clientX;
+  pt.y = evt.clientY;
+  const scaled = pt.matrixTransform(svg.getScreenCTM()!.inverse());
+  return new Complex(scaled.x, -scaled.y);
+}
+
+svg.addEventListener("mouseenter", (event) => {
+  // TODO delete me
+  //console.log(event);
+});
+svg.addEventListener("mouseleave", (event) => {
+  // TODO undo all of the recent changes.
+  //console.log(event);
+});
+svg.addEventListener("mousemove", (event) => {
+  //console.log(event);
+  // TODO move the updateZ() to here.
+});
+svg.addEventListener("mouseup", (event) => {
+  //console.log(event);
+  //console.log(cursorToComplex(event));
+  updateZ(cursorToComplex(event));
+  // TODO this should save the current position.
+  // Save doesn't exist yet!
+});
+
+//let z = formula.initialZ;
